a9253055b96b36ab183b7cfedadeaa5d9719e3b3
Mordy Pesach <mordypassover@gmail.com>
2026-05-13 17:49:08 +0300

    update update_lists, get_letter funk

--- hang_man.py
+++ hang_man.py
@@ -24,9 +24,15 @@ def reconnect_list_to_string(letter_list):
 def show_game_status(cnt,hiden_split_word,wrong_letters):
     print(f"trys left: {cnt}, {reconnect_list_to_string(hiden_split_word)} , words used {wrong_letters} ")
 
-def get_letter(wrong_letters=[None]):
+def get_letter(wrong_letters, hiden_list):
     user_letter= input("please enter a letter: ")
-    return user_letter if user_letter.isalpha() and len(user_letter)==1 and user_letter not in wrong_letters  else (print("bad input"))
+    if user_letter.isalpha() and (len(user_letter) == 1) and (user_letter not in wrong_letters) and user_letter not in  hiden_list:
+        return user_letter
+    else:
+        (print("bad input"))
+        update_wrong_letters_list(user_letter, wrong_letters)
+        get_letter(wrong_letters, hiden_list)
+
 
 
 def check_gues(letter,word,):
@@ -43,12 +49,16 @@ def update_wrong_letters_list(letter,lst):
 
 def update_lists(letter,word,hiden_list,wrong_letters_list):
     is_good_gues=check_gues(letter,word)
-    return update_hiden_list(letter,word,hiden_list) if is_good_gues else  update_wrong_letters_list(letter,wrong_letters_list)
+    if is_good_gues:
+         update_hiden_list(letter,word,hiden_list)
+    else:
+        update_wrong_letters_list(letter,wrong_letters_list)
+    return
 
 
 def main():
-    TRY_CNT=10
-    word_list=['wert','qwertyh','wertgv']
+    TRY_CNT = 10
+    word_list = ['wert', 'qwertyh','wertgv']
     word=choose_game_word_from_list(word_list)
     hiden_letters_list=hiden_word_in_list(word)
     wrong_letters=[]
@@ -57,7 +67,8 @@ def main():
 
     while TRY_CNT > 0 and "_"  in hiden_letters_list:
         show_game_status(TRY_CNT, hiden_letters_list,wrong_letters)
-        user_letter = get_letter(wrong_letters)
+        user_letter = get_letter(wrong_letters, hiden_letters_list)
 
         update_lists(user_letter,word,hiden_letters_list,wrong_letters)
         TRY_CNT-=1
+main()
