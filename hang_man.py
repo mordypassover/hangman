2a6d49a39c679833ac5c8f523bd57dabaa89dd82
Mordy Pesach <mordypassover@gmail.com>
2026-05-13 15:13:13 +0300

    add main file & func structcher

---
+++ hang_man.py
@@ -0,0 +1,21 @@
+def choose_game_word_from_list(lst):
+    pass
+
+def hied_word(word):
+    pass
+
+def split_string(string):
+    pass
+
+def reconnect_string():
+    pass
+
+def show_game_status():
+    pass
+
+def get_letter():
+    pass
+
+def main():
+    pass
+
