c1e4dffea8b4b095fc5f1749021838f1852da540
Mordy Pesach <mordypassover@gmail.com>
2026-05-14 10:58:23 +0300

    fix spelling mistakes

--- hang_man.py
+++ hang_man.py
@@ -11,9 +11,9 @@ def hied_word(word):
 def split_string_to_list(string):
     return list(string)
 
-def hiden_word_in_list(word):
-    hiden_word=hied_word(word)
-    return split_string_to_list(hiden_word)
+def hidden_word_in_list(word):
+    hidden_word=hied_word(word)
+    return split_string_to_list(hidden_word)
 
 def reconnect_list_to_string(letter_list):
     reconnected=""
@@ -21,44 +21,44 @@ def reconnect_list_to_string(letter_list):
         reconnected+=i
     return reconnected
 
-def show_game_status(cnt,hiden_split_word,wrong_letters):
-    print(f"trys left: {cnt}, {reconnect_list_to_string(hiden_split_word)} , words used: {", ".join(wrong_letters)} ")
+def show_game_status(cnt,hidden_split_word,wrong_letters):
+    print(f"tris left: {cnt}, {reconnect_list_to_string(hidden_split_word)} , words used: {", ".join(wrong_letters)} ")
 
-def get_letter(wrong_letters, hiden_list):
+def get_letter(wrong_letters, hidden_list):
     user_letter= input("please enter a letter: ")
 
-    if not (user_letter.isalpha() and (len(user_letter) == 1) and (user_letter not in wrong_letters)) or user_letter  in  hiden_list:
+    if not (user_letter.isalpha() and (len(user_letter) == 1) and (user_letter not in wrong_letters)) or user_letter  in  hidden_list:
         (print("bad input"))
-        update_wrong_letters_list(user_letter, wrong_letters,hiden_list)
-        return get_letter(wrong_letters, hiden_list)
+        update_wrong_letters_list(user_letter, wrong_letters,hidden_list)
+        return get_letter(wrong_letters, hidden_list)
     return user_letter
 
 
 def check_gues(letter,word,):
     return letter in list(word)
 
-def update_hiden_list(letter,word,lst):
+def update_hidden_list(letter,word,lst):
     for index, l in enumerate(word) :
         if letter == l:
             lst[index]=letter
     return lst
 
-def update_wrong_letters_list(letter,wrong_list,hiden_list ):
-    if (letter not in hiden_list)  and len(letter) == 1:
+def update_wrong_letters_list(letter,wrong_list,hidden_list ):
+    if (letter not in hidden_list)  and len(letter) == 1:
          wrong_list.add(letter)
     return wrong_list
 
-def update_lists(letter,word,hiden_list,wrong_letters_list):
+def update_lists(letter,word,hidden_list,wrong_letters_list):
     is_good_gues=check_gues(letter,word)
     if is_good_gues:
-         update_hiden_list(letter,word,hiden_list)
+         update_hidden_list(letter,word,hidden_list)
     else:
-        update_wrong_letters_list(letter,wrong_letters_list,hiden_list)
+        update_wrong_letters_list(letter,wrong_letters_list,hidden_list)
     return
 
-def game_end(hidenw_ordes,word ,trys):
-    if hidenw_ordes == list(word):
-        print(f"congratulations! you won with {trys} left!")
+def game_end(hidden_words,word ,tris):
+    if hidden_words == list(word):
+        print(f"congratulations! word:{word} you won with {tris} left!")
     else:
         print("you loose!")
 
@@ -68,17 +68,20 @@ def main():
     TRY_CNT = 10
     word_list = ['wert', 'qwertyh','wertgv']
     word=choose_game_word_from_list(word_list)
-    hiden_letters_list=hiden_word_in_list(word)
+    hidden_letters_list=hidden_word_in_list(word)
     wrong_letters=set()
 
 
 
-    while TRY_CNT > 0 and "_"  in hiden_letters_list:
-        show_game_status(TRY_CNT, hiden_letters_list,wrong_letters)
-        user_letter = get_letter(wrong_letters, hiden_letters_list)
+    while TRY_CNT > 0 and "_"  in hidden_letters_list:
+        show_game_status(TRY_CNT, hidden_letters_list,wrong_letters)
+        user_letter = get_letter(wrong_letters, hidden_letters_list)
 
-        update_lists(user_letter,word,hiden_letters_list,wrong_letters)
+        update_lists(user_letter,word,hidden_letters_list,wrong_letters)
         TRY_CNT-=1
 
-    show_game_status(TRY_CNT, hiden_letters_list,wrong_letters)
-    game_end(hiden_letters_list, word,TRY_CNT)
+    show_game_status(TRY_CNT, hidden_letters_list,wrong_letters)
+    game_end(hidden_letters_list, word,TRY_CNT)
+
+if __name__ == "__main__":
+    main()
